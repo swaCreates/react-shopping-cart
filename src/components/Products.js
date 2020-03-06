6202f9910fdab1e9956eecdf569baba9100aea54
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-03-05 20:14:55 -0500

    completed creating the ProductContext object

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 // Components
 import Product from './Product';
 
-const Products = props => {
+import {ProductContext} from '../context/ProductContext';
+
+// Now that our ProductContext is now providing data we can finally consume it! 
+// To do so let's head over to our Products component and import the useContext hook as well as our ProductContext.
+
+// In the component, call the useContext hook and pass in the context object we want to use into it.
+
+// When we do this, useContext is going to return value passed by our ProductContext Provider value prop.
+//  In our case we're getting back an object with two properties. 
+// A products property and a addItem property. We can go ahead and destructure those.
+
+const Products = () => {
+
+	const {products, addItem}= useContext(ProductContext);
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
